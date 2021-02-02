60f6193b9fff75e093dfa8f857cc4921d48e6741
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-02 14:29:57 +0300

    updates

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,9 +3,12 @@
 # выхожу, если одна из команд завершилась неудачно
 set -e
 
+git remote add origin "https://reme3d2y:$PERMISSION_GITHUB_TOKEN@github.com/test-rpackage-box.git"
 git checkout master
 git branch -u origin/master
 git fetch --tags
+git remote -v
+
 
 # поднимаю версию во всех подпакетах
 lerna version --conventional-commits --no-commit-hooks --amend --yes
