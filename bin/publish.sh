d7bccfac44002a0f65226e9fb72bdcac3de7bf37
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-02 13:12:53 +0300

    try #7

--- bin/publish.sh
+++ bin/publish.sh
@@ -9,6 +9,6 @@ git fetch --tags
 # поднимаю версию во всех подпакетах
 lerna version --conventional-commits --no-commit-hooks --amend --yes
 # публикую все подпакеты
-lerna publish from-git
+lerna publish from-git --yes
 # отправляю изменения на github
 # git push --follow-tags
