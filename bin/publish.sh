eb5bc5d92981d7f8438c007c5afcb768c7fa93ba
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-03 17:29:37 +0300

    updates

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,17 +6,12 @@ set -e
 git remote set-url origin https://reme3d2y:$GITHUB_TOKEN@github.com/reme3d2y/test-rpackage-box.git
 git checkout master
 git fetch --tags
-git status
-git branch
-git log -5
-git remote show origin
-
 
 # поднимаю версию во всех подпакетах
-lerna version --conventional-commits --no-commit-hooks --amend --yes
+lerna version --conventional-commits --no-commit-hooks --yes
 
 # отправляю изменения на github
 git push origin master
 
 # публикую все подпакеты
-# lerna publish from-git --yes
+lerna publish from-git --yes
