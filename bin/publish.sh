b75fa9e8d20d114c1e0284ad030dd3ab461eab4b
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-03 11:58:04 +0300

    updates

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,13 +7,11 @@ git remote set-url origin https://reme3d2y:$GITHUB_TOKEN@github.com/reme3d2y/tes
 git checkout master
 git branch -u origin/master
 git fetch --tags
-git fetch --prune --unshallow
-git pull --rebase
 
 
 # поднимаю версию во всех подпакетах
 lerna version --conventional-commits --no-commit-hooks --amend --yes
 # публикую все подпакеты
-# lerna publish from-git --yes
+lerna publish from-git --yes
 # отправляю изменения на github
-git push --follow-tags
+git push
