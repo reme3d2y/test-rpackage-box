d23ce11437ccde9277a1e6a86c1c508cfb84f675
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-03 16:57:04 +0300

    updates

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,9 +4,11 @@
 set -e
 
 git remote set-url origin https://reme3d2y:$GITHUB_TOKEN@github.com/reme3d2y/test-rpackage-box.git
-git fetch --tags --update-head-ok https://reme3d2y:$GITHUB_TOKEN@github.com/reme3d2y/test-rpackage-box.git
+git checkout master
+git fetch --tags
 git status
 git branch
+git log -5
 
 
 # поднимаю версию во всех подпакетах
