176f29e8692f10422bf012c746c61dbc24f57bb8
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-04 15:58:02 +0300

    fixes

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# выхожу, если одна из команд завершилась неудачно
 set -e
 
 git remote set-url origin https://semantic-release-bot:$GITHUB_TOKEN@github.com/reme3d2y/test-rpackage-box.git
@@ -8,17 +7,11 @@ git checkout master
 git pull origin master
 git fetch --tags
 
-changed_packages='$(lerna changed)'
-
-# поднимаю версию во всех подпакетах
-lerna version --conventional-commits --no-commit-hooks --yes
-
-git push origin master
-
-if [ -z "$changed_packages" ]; then
+if [ -z $(lerna changed) ]
+then
     echo "There are no relevant changes, so no new versions are released."
 else
-    echo "Publish subpackages:"
-    echo $changed_packages
+    lerna version --conventional-commits --no-commit-hooks --yes
+    git push origin master
     lerna publish from-git --yes
 fi
