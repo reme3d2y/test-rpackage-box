e171cf25735762ea873f559a4fd1f5c913549328
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-04 11:44:20 +0300

    fixes

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,19 +7,17 @@ git remote set-url origin https://reme3d2y:$GITHUB_TOKEN@github.com/reme3d2y/tes
 git checkout master
 git fetch --tags
 
+changed_packages='$(lerna changed)'
+
 # поднимаю версию во всех подпакетах
 lerna version --conventional-commits --no-commit-hooks --yes
 
 # отправляю изменения на github
 git push origin master
 
-changed_packages=`lerna changed`;
-
-if [ -z "$changed_packages" ]
-then
+if [ -z "$changed_packages" ]; then
     echo "There are no relevant changes, so no new versions are released."
-    exit 0
 else
-    # публикую все подпакеты
-    lerna publish from-git --yes  
+    echo "Publish subpackages."
+    lerna publish from-git --yes
 fi
