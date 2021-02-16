7730f43fb1d2c9b45e6a49e1176b126f07633a97
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-16 16:59:17 +0300

    fixes

--- bin/publish.sh
+++ bin/publish.sh
@@ -9,10 +9,8 @@ echo $semantic_output
 
 # Проеряем, что semantic-release зарелизил рут-пакет.
 # Пока не знаю, как это можно сделать по-нормальному.
-if [[ $semantic_output =~ "new version won't be published" ]]
+if [[ $semantic_output =~ "Publishing version" ]]
 then
-    exit 0
-else
     git remote set-url origin https://semantic-release-bot:$GITHUB_TOKEN@github.com/reme3d2y/test-rpackage-box.git
     git checkout master
     git pull origin master --rebase
@@ -25,4 +23,6 @@ else
         git push origin master
         lerna publish from-git --yes
     fi
+else
+    exit 0
 fi
