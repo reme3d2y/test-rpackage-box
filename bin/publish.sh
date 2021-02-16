6295dc592aaf4cee809c43d1143751367d5be883
dmitrsavk <dmitrsavk@yandex.ru>
2021-02-16 16:38:31 +0300

    fixes

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,16 +2,25 @@
 
 set -e
 
-git remote set-url origin https://semantic-release-bot:$GITHUB_TOKEN@github.com/reme3d2y/test-rpackage-box.git
-git checkout master
-git pull origin master --rebase
-git fetch --tags
+# Релизим рут-пакет с помощью semantic-release.
+semantic_output=$(yarn semantic-release)
 
-if [ -z $(lerna changed) ]
+# Проеряем, что semantic-release зарелизил рут-пакет.
+# Пока не знаю, как это можно сделать по-нормальному.
+if [[ $semantic_output =~ "new version won't be published" ]]
 then
-    echo "There are no relevant changes, so no new versions are released."
+    exit 0
 else
-    lerna version --conventional-commits --no-commit-hooks --yes
-    git push origin master
-    lerna publish from-git --yes
+    git remote set-url origin https://semantic-release-bot:$GITHUB_TOKEN@github.com/reme3d2y/test-rpackage-box.git
+    git checkout master
+    git pull origin master --rebase
+    git fetch --tags
+
+    if [ -z $(lerna changed) ]; then
+        echo "There are no relevant changes, so no new versions are released."
+    else
+        lerna version --conventional-commits --no-commit-hooks --yes
+        git push origin master
+        lerna publish from-git --yes
+    fi
 fi
